3319ca47b181452a53fa1c50df57059aa24de1b4
weizongwei5 <weizongwei5@gmail.com>
2017-01-02 13:04:53 +0800

    parceler

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 [https://github.com/immutables/immutables](https://github.com/immutables/immutables)
 
+
+[https://github.com/johncarl81/parceler](https://github.com/johncarl81/parceler)
+
+parceler这个库可以不用写这些代码，但是无法用在immutables上，虽然也可以用在auto-value上，但是他始终没有auto-value-parcel好用，所以我觉得这个库用起来比较鸡肋。
--- app/src/main/java/com/androidyuan/java_code_clean_lib_demo/model/Student4.java
+++ app/src/main/java/com/androidyuan/java_code_clean_lib_demo/model/Student4.java
@@ -1,16 +1,20 @@
 package com.androidyuan.java_code_clean_lib_demo.model;
 
 import org.immutables.value.Value;
+import org.parceler.Parcel;
 
 /**
  * Created by wei on 17-1-2.
  */
 
 @Value.Immutable
+@Parcel
 public abstract class Student4 {
 
 
     public abstract int age();
 
     public abstract String name();
+
+
 }
