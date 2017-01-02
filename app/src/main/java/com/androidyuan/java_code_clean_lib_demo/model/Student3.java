0c3ff086f4528c9746d38c3c82416187b740d500
weizongwei5 <weizongwei5@gmail.com>
2017-01-02 11:03:34 +0800

    一个可run的版本

--- app/src/main/java/com/androidyuan/java_code_clean_lib_demo/model/Student3.java
+++ app/src/main/java/com/androidyuan/java_code_clean_lib_demo/model/Student3.java
@@ -8,13 +8,13 @@ import com.google.auto.value.AutoValue;
  * Created by wei on 16-12-30.
  */
 @AutoValue
-public abstract class Student3 implements Parcelable{
+public abstract class Student3 implements Parcelable {
+
+    public static Student3 create(int age, String name) {
+        return new AutoValue_Student3(age, name);//这里 可能会报错  可能你没有配置apt 的依赖
+    }
 
     public abstract int age();
 
     public abstract String name();
-
-    public static Student3 create(int age,String name){
-        return new AutoValue_Student3(age,name);//这里 可能会报错 不要在意 会让你编译通过
-    }
 }
